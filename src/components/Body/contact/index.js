0106bd955944334935b2d5db1a9e35bad7e58840
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-09-06 12:19:46 -0300

    centralizado container contact

--- src/components/Body/contact/index.js
+++ src/components/Body/contact/index.js
@@ -8,13 +8,11 @@ const Contact = () => (
     <Separator />
     <h1 className="section-title">Contact</h1>
     <div className="contact-container">
-      <div className="contact-left">
-        <p>
-          Do you want to get in touch ?
-          Click on one of the icons, I&apos;ll be happy to answer it
-        </p>
-        <SocialContact />
-      </div>
+      <p>
+        Do you want to get in touch ?
+        Click on one of the icons, I&apos;ll be happy to answer it
+      </p>
+      <SocialContact />
     </div>
   </div>
 );
